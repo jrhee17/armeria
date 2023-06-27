0108784a36ef6565eb5dc70a2d1202839b5acd33
Hannam Rhee <jrhee17@linecorp.com>
2023-06-27 12:01:09 +0900

    poc

--- examples/spring-boot-tomcat/src/main/java/example/springframework/boot/tomcat/HelloConfiguration.java
+++ examples/spring-boot-tomcat/src/main/java/example/springframework/boot/tomcat/HelloConfiguration.java
@@ -1,8 +1,19 @@
 package example.springframework.boot.tomcat;
 
 import org.apache.catalina.connector.Connector;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.coyote.UpgradeProtocol;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.SearchStrategy;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatWebServer;
 import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +22,8 @@ import com.linecorp.armeria.server.ServerBuilder;
 import com.linecorp.armeria.server.healthcheck.HealthChecker;
 import com.linecorp.armeria.server.tomcat.TomcatService;
 import com.linecorp.armeria.spring.ArmeriaServerConfigurator;
+
+import jakarta.servlet.Servlet;
 
 /**
  * Configures an Armeria {@link Server} to redirect the incoming requests to the Tomcat instance provided by
@@ -26,7 +39,7 @@ public class HelloConfiguration {
         final TomcatWebServer container = (TomcatWebServer) applicationContext.getWebServer();
 
         // Start the container to make sure all connectors are available.
-        container.start();
+//        container.start();
         return container.getTomcat().getConnector();
     }
 
@@ -55,5 +68,37 @@ public class HelloConfiguration {
     @Bean
     public ArmeriaServerConfigurator armeriaServiceInitializer(TomcatService tomcatService) {
         return sb -> sb.serviceUnder("/", tomcatService);
+    }
+
+    @Configuration(proxyBeanMethods = false)
+    @ConditionalOnClass({ Servlet.class, Tomcat.class, UpgradeProtocol.class })
+    @ConditionalOnMissingBean(value = ServletWebServerFactory.class, search = SearchStrategy.CURRENT)
+    static class EmbeddedTomcat {
+
+        @Bean
+        TomcatServletWebServerFactory tomcatServletWebServerFactory(
+                ObjectProvider<TomcatConnectorCustomizer> connectorCustomizers,
+                ObjectProvider<TomcatContextCustomizer> contextCustomizers,
+                ObjectProvider<TomcatProtocolHandlerCustomizer<?>> protocolHandlerCustomizers) {
+            NoPortTomcatServletWebServerFactory factory = new NoPortTomcatServletWebServerFactory();
+            factory.doSetPort();
+            factory.getTomcatConnectorCustomizers().addAll(connectorCustomizers.orderedStream().toList());
+            factory.getTomcatContextCustomizers().addAll(contextCustomizers.orderedStream().toList());
+            factory.getTomcatProtocolHandlerCustomizers().addAll(
+                    protocolHandlerCustomizers.orderedStream().toList());
+            return factory;
+        }
+
+        private static class NoPortTomcatServletWebServerFactory extends TomcatServletWebServerFactory {
+            @Override
+            public void setPort(int port) {
+                // no-op
+            }
+
+            void doSetPort() {
+                super.setPort(-1);
+            }
+
+        }
     }
 }
