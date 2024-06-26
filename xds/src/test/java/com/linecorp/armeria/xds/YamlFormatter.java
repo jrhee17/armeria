33bbd5c385c2f84596829fa3755c4b768dfc22c9
jrhee17 <guins_j@guins.org>
2024-06-26 15:51:22 +0900

    impl yaml formatter

--- xds/src/test/java/com/linecorp/armeria/xds/YamlFormatter.java
+++ xds/src/test/java/com/linecorp/armeria/xds/YamlFormatter.java
@@ -16,10 +16,43 @@
 
 package com.linecorp.armeria.xds;
 
-import java.io.File;
+import static com.google.common.base.Preconditions.checkNotNull;
 
-public class YamlFormatter {
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-    YamlFormatter(File yamlFile) {
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.google.protobuf.util.JsonFormat;
+import com.google.protobuf.util.JsonFormat.Parser;
+import com.google.protobuf.util.JsonFormat.TypeRegistry;
+
+import io.envoyproxy.envoy.config.listener.v3.Listener;
+import io.envoyproxy.envoy.extensions.filters.http.router.v3.Router;
+import io.envoyproxy.envoy.extensions.filters.network.http_connection_manager.v3.HttpConnectionManager;
+
+public final class YamlFormatter {
+
+    private static final ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+    private static final Parser parser =
+            JsonFormat.parser().usingTypeRegistry(TypeRegistry.newBuilder()
+                                                              .add(HttpConnectionManager.getDescriptor())
+                                                              .add(Router.getDescriptor())
+                                                              .build());
+
+    public static Listener formatResource(String resourceName) throws IOException, URISyntaxException {
+        final URL resource = YamlFormatter.class.getResource(resourceName);
+        checkNotNull(resource, "Couldn't find resource (%s)", resourceName);
+        final byte[] bytes = Files.readAllBytes(Paths.get(resource.toURI()));
+        final JsonNode jsonNode = mapper.reader().readTree(bytes);
+        final Listener.Builder builder = Listener.newBuilder();
+        parser.merge(jsonNode.toString(), builder);
+        return builder.build();
     }
+
+    private YamlFormatter() {}
 }
