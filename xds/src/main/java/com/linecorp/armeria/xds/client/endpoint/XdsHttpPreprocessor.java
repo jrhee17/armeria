2cb0e561d7a8717f02b4bfa1ebfb01015d815a27
jrhee17 <guins_j@guins.org>
2025-02-03 18:26:52 +0900

    refactor

--- xds/src/main/java/com/linecorp/armeria/xds/client/endpoint/XdsHttpPreprocessor.java
+++ xds/src/main/java/com/linecorp/armeria/xds/client/endpoint/XdsHttpPreprocessor.java
@@ -16,26 +16,16 @@
 
 package com.linecorp.armeria.xds.client.endpoint;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionException;
-
 import com.linecorp.armeria.client.HttpPreprocessor;
-import com.linecorp.armeria.client.PreClient;
-import com.linecorp.armeria.client.PreClientRequestContext;
 import com.linecorp.armeria.common.HttpRequest;
 import com.linecorp.armeria.common.HttpResponse;
-import com.linecorp.armeria.common.TimeoutException;
-import com.linecorp.armeria.common.annotation.Nullable;
-import com.linecorp.armeria.xds.ListenerRoot;
-import com.linecorp.armeria.xds.ListenerSnapshot;
 import com.linecorp.armeria.xds.XdsBootstrap;
-
-import io.netty.channel.EventLoop;
 
 /**
  * TBU.
  */
-public final class XdsHttpPreprocessor implements HttpPreprocessor, AutoCloseable {
+public final class XdsHttpPreprocessor extends XdsPreprocessor<HttpRequest, HttpResponse>
+        implements HttpPreprocessor {
 
     /**
      * TBU.
@@ -44,50 +34,9 @@ public final class XdsHttpPreprocessor implements HttpPreprocessor, AutoCloseabl
         return new XdsHttpPreprocessor(listenerName, xdsBootstrap);
     }
 
-    private final ListenerRoot listenerRoot;
-    private final SnapshotWatcherSelector snapshotWatcherSelector;
-    private final String listenerName;
-
     private XdsHttpPreprocessor(String listenerName, XdsBootstrap xdsBootstrap) {
-        this.listenerName = listenerName;
-        listenerRoot = xdsBootstrap.listenerRoot(listenerName);
-        snapshotWatcherSelector = new SnapshotWatcherSelector(listenerRoot);
-    }
-
-    @Override
-    public HttpResponse execute(PreClient<HttpRequest, HttpResponse> delegate,
-                                PreClientRequestContext ctx, HttpRequest req) throws Exception {
-        final ListenerSnapshot listenerSnapshot = snapshotWatcherSelector.selectNow(ctx);
-        if (listenerSnapshot != null) {
-            return execute0(delegate, ctx, req, listenerSnapshot);
-        }
-        final EventLoop temporaryEventLoop = ctx.options().factory().eventLoopSupplier().get();
-        final CompletableFuture<HttpResponse> resFuture =
-                snapshotWatcherSelector.select(ctx, temporaryEventLoop, ctx.responseTimeoutMillis())
-                                       .thenApply(clusterEntries0 -> {
-                                           try {
-                                               return execute0(delegate, ctx, req, clusterEntries0);
-                                           } catch (Exception e) {
-                                               throw new CompletionException(e);
-                                           }
-                                       });
-        return HttpResponse.of(resFuture);
-    }
-
-    private HttpResponse execute0(PreClient<HttpRequest, HttpResponse> delegate,
-                                  PreClientRequestContext ctx, HttpRequest req,
-                                  @Nullable ListenerSnapshot listenerSnapshot) throws Exception {
-        if (listenerSnapshot == null) {
-            throw new TimeoutException("Couldn't select a snapshot for listener '" +
-                                       listenerName + "'.");
-        }
-        final RouteConfig routeConfig = new RouteConfig(listenerSnapshot);
-        ctx.setAttr(XdsFilterAttributeKeys.ROUTE_CONFIG, routeConfig);
-        return routeConfig.downstreamFilters().httpPreprocessor().execute(delegate, ctx, req);
-    }
-
-    @Override
-    public void close() {
-        listenerRoot.close();
+        super(listenerName, xdsBootstrap, HttpResponse::of,
+              (xdsFilter, preClient) -> (ctx, req) -> xdsFilter.httpPreprocessor()
+                                                               .execute(preClient, ctx, req));
     }
 }
--- xds/src/main/java/com/linecorp/armeria/xds/client/endpoint/XdsRpcPreprocessor.java
+++ xds/src/main/java/com/linecorp/armeria/xds/client/endpoint/XdsRpcPreprocessor.java
@@ -16,26 +16,16 @@
 
 package com.linecorp.armeria.xds.client.endpoint;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionException;
-
-import com.linecorp.armeria.client.PreClient;
-import com.linecorp.armeria.client.PreClientRequestContext;
 import com.linecorp.armeria.client.RpcPreprocessor;
 import com.linecorp.armeria.common.RpcRequest;
 import com.linecorp.armeria.common.RpcResponse;
-import com.linecorp.armeria.common.TimeoutException;
-import com.linecorp.armeria.common.annotation.Nullable;
-import com.linecorp.armeria.xds.ListenerRoot;
-import com.linecorp.armeria.xds.ListenerSnapshot;
 import com.linecorp.armeria.xds.XdsBootstrap;
-
-import io.netty.channel.EventLoop;
 
 /**
  * TBU.
  */
-public final class XdsRpcPreprocessor implements RpcPreprocessor, AutoCloseable {
+public final class XdsRpcPreprocessor extends XdsPreprocessor<RpcRequest, RpcResponse>
+        implements RpcPreprocessor {
 
     /**
      * TBU.
@@ -44,50 +34,9 @@ public final class XdsRpcPreprocessor implements RpcPreprocessor, AutoCloseable 
         return new XdsRpcPreprocessor(listenerName, xdsBootstrap);
     }
 
-    private final ListenerRoot listenerRoot;
-    private final SnapshotWatcherSelector snapshotWatcherSelector;
-    private final String listenerName;
-
     private XdsRpcPreprocessor(String listenerName, XdsBootstrap xdsBootstrap) {
-        this.listenerName = listenerName;
-        listenerRoot = xdsBootstrap.listenerRoot(listenerName);
-        snapshotWatcherSelector = new SnapshotWatcherSelector(listenerRoot);
-    }
-
-    @Override
-    public RpcResponse execute(PreClient<RpcRequest, RpcResponse> delegate,
-                               PreClientRequestContext ctx, RpcRequest req) throws Exception {
-        final ListenerSnapshot listenerSnapshot = snapshotWatcherSelector.selectNow(ctx);
-        if (listenerSnapshot != null) {
-            return execute0(delegate, ctx, req, listenerSnapshot);
-        }
-        final EventLoop temporaryEventLoop = ctx.options().factory().eventLoopSupplier().get();
-        final CompletableFuture<RpcResponse> resFuture =
-                snapshotWatcherSelector.select(ctx, temporaryEventLoop, ctx.responseTimeoutMillis())
-                                       .thenApply(listenerSnapshot0 -> {
-                                           try {
-                                               return execute0(delegate, ctx, req, listenerSnapshot0);
-                                           } catch (Exception e) {
-                                               throw new CompletionException(e);
-                                           }
-                                       });
-        return RpcResponse.of(resFuture);
-    }
-
-    private RpcResponse execute0(PreClient<RpcRequest, RpcResponse> delegate,
-                                 PreClientRequestContext ctx, RpcRequest req,
-                                 @Nullable ListenerSnapshot listenerSnapshot) throws Exception {
-        if (listenerSnapshot == null) {
-            throw new TimeoutException("Couldn't select a snapshot for listener '" +
-                                       listenerName + "'.");
-        }
-        final RouteConfig routeConfig = new RouteConfig(listenerSnapshot);
-        ctx.setAttr(XdsFilterAttributeKeys.ROUTE_CONFIG, routeConfig);
-        return routeConfig.downstreamFilters().rpcPreprocessor().execute(delegate, ctx, req);
-    }
-
-    @Override
-    public void close() {
-        listenerRoot.close();
+        super(listenerName, xdsBootstrap, RpcResponse::of,
+              (xdsFilter, preClient) -> (ctx, req) -> xdsFilter.rpcPreprocessor()
+                                                               .execute(preClient, ctx, req));
     }
 }
