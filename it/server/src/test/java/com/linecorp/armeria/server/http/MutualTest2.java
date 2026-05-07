e2537cf12d8615ef4bf0fe303496e96096751983
jrhee17 <guins_j@guins.org>
2026-05-07 15:06:10 +0900

    poc

---
+++ it/server/src/test/java/com/linecorp/armeria/server/http/MutualTest2.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright 2018 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.linecorp.armeria.server.http;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import com.linecorp.armeria.client.BlockingWebClient;
+import com.linecorp.armeria.client.ClientFactory;
+import com.linecorp.armeria.client.ClientTlsConfig;
+import com.linecorp.armeria.client.WebClient;
+import com.linecorp.armeria.client.logging.LoggingClient;
+import com.linecorp.armeria.common.HttpResponse;
+import com.linecorp.armeria.common.HttpStatus;
+import com.linecorp.armeria.common.TlsKeyPair;
+import com.linecorp.armeria.common.TlsProvider;
+import com.linecorp.armeria.common.util.TlsEngineType;
+import com.linecorp.armeria.server.ServerBuilder;
+import com.linecorp.armeria.server.ServerTlsConfig;
+import com.linecorp.armeria.server.logging.LoggingService;
+import com.linecorp.armeria.testing.junit5.server.SelfSignedCertificateExtension;
+import com.linecorp.armeria.testing.junit5.server.ServerExtension;
+import com.linecorp.armeria.testing.junit5.server.SignedCertificateExtension;
+
+import io.netty.handler.ssl.ClientAuth;
+
+class MutualTlsTest2 {
+
+    @Order(1)
+    @RegisterExtension
+    static final SelfSignedCertificateExtension serverCert1 = new SelfSignedCertificateExtension();
+
+    @Order(2)
+    @RegisterExtension
+    static final SignedCertificateExtension serverCert2 = new SignedCertificateExtension(serverCert1);
+
+    @Order(3)
+    @RegisterExtension
+    static final SignedCertificateExtension serverCert3 = new SignedCertificateExtension(serverCert2);
+
+    @Order(4)
+    @RegisterExtension
+    static final SelfSignedCertificateExtension clientCert1 = new SelfSignedCertificateExtension();
+
+    @Order(5)
+    @RegisterExtension
+    static final SignedCertificateExtension clientCert2 = new SignedCertificateExtension(clientCert1);
+
+    @Order(6)
+    @RegisterExtension
+    static final SignedCertificateExtension clientCert3 = new SignedCertificateExtension(clientCert2);
+
+    @RegisterExtension
+    static ServerExtension jdkServer = new ServerExtension() {
+        @Override
+        protected void configure(ServerBuilder sb) throws Exception {
+            configureServer(sb, TlsEngineType.JDK);
+        }
+    };
+
+    @RegisterExtension
+    static ServerExtension opensslServer = new ServerExtension() {
+        @Override
+        protected void configure(ServerBuilder sb) throws Exception {
+            configureServer(sb, TlsEngineType.OPENSSL);
+        }
+    };
+
+    private static void configureServer(ServerBuilder sb, TlsEngineType engineType) {
+        final TlsKeyPair tlsKeyPair = TlsKeyPair.of(serverCert3.privateKey(),
+                                                    serverCert3.certificate());
+        sb.tlsEngineType(engineType);
+        sb.tlsProvider(TlsProvider.of(tlsKeyPair),
+                       ServerTlsConfig.builder()
+                                      .clientAuth(ClientAuth.REQUIRE)
+                                      .tlsCustomizer(b -> b.trustManager(clientCert2.certificate()))
+                                      .build());
+
+        sb.service("/", (ctx, req) -> HttpResponse.of("success"));
+        sb.decorator(LoggingService.builder().newDecorator());
+    }
+
+    @ParameterizedTest
+    @EnumSource(TlsEngineType.class)
+    void intermediateOnlyTrustAnchor(TlsEngineType engineType) {
+        final ServerExtension server = engineType == TlsEngineType.JDK ? jdkServer : opensslServer;
+        final TlsKeyPair clientKeyPair = TlsKeyPair.of(clientCert3.privateKey(),
+                                                       clientCert3.certificate());
+        final ClientTlsConfig tlsConfig =
+                ClientTlsConfig.builder()
+                               .tlsCustomizer(b -> b.trustManager(serverCert2.certificate()))
+                               .build();
+        try (ClientFactory clientFactory =
+                     ClientFactory.builder()
+                                  .tlsEngineType(engineType)
+                                  .tlsProvider(TlsProvider.of(clientKeyPair), tlsConfig)
+                                  .build()) {
+            final BlockingWebClient client = WebClient.builder(server.httpsUri())
+                                                      .factory(clientFactory)
+                                                      .decorator(LoggingClient.newDecorator())
+                                                      .build()
+                                                      .blocking();
+            assertThat(client.get("/").status()).isEqualTo(HttpStatus.OK);
+        }
+    }
+}
